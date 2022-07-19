ee635204f43dbe0ccee51a13379c9db2340af5bd
高怡飞 <49179966+Gaoyifei1011@users.noreply.github.com>
2022-07-19 21:01:55 +0800

    Update 20220719
    Update 20220719

---
+++ UI/Controls/About/SettingsHelpControl.xaml.cs
@@ -0,0 +1,20 @@
+﻿using GetStoreApp.Contracts.Services.App;
+using GetStoreApp.ViewModels.Controls.About;
+using Microsoft.UI.Xaml.Controls;
+
+namespace GetStoreApp.UI.Controls.About
+{
+    public sealed partial class SettingsHelpControl : UserControl
+    {
+        public IResourceService ResourceService { get; }
+
+        public SettingsHelpViewModel ViewModel { get; }
+
+        public SettingsHelpControl()
+        {
+            ResourceService = App.GetService<IResourceService>();
+            ViewModel = App.GetService<SettingsHelpViewModel>();
+            this.InitializeComponent();
+        }
+    }
+}
--- ViewModels/Controls/Settings/LinkFilterViewModel.cs
+++ ViewModels/Controls/Settings/LinkFilterViewModel.cs
@@ -2,14 +2,19 @@
 using CommunityToolkit.Mvvm.Input;
 using CommunityToolkit.Mvvm.Messaging;
 using GetStoreApp.Contracts.Services.Settings;
+using GetStoreApp.Contracts.Services.Shell;
 using GetStoreApp.Messages;
+using GetStoreApp.ViewModels.Pages;
+using Microsoft.UI.Xaml.Media.Animation;
 using System;
+using System.Threading.Tasks;
 
 namespace GetStoreApp.ViewModels.Controls.Settings
 {
     public class LinkFilterViewModel : ObservableRecipient
     {
         private readonly ILinkFilterService LinkFilterService;
+        private readonly INavigationService NavigationService;
 
         private bool _startsWithEFilterValue;
 
@@ -29,26 +34,25 @@ namespace GetStoreApp.ViewModels.Controls.Settings
             set { SetProperty(ref _blockMapFilterValue, value); }
         }
 
+        public IAsyncRelayCommand LinkFilterInstructionCommand { get; set; }
+
         public IAsyncRelayCommand StartWithEFilterCommand { get; set; }
 
         public IAsyncRelayCommand BlockMapFilterCommand { get; set; }
 
-        public IAsyncRelayCommand StartsWithECommand { get; set; } = new AsyncRelayCommand(async () =>
-        {
-            await Windows.System.Launcher.LaunchUriAsync(new Uri("https://datatypes.net/open-eappx-files#:~:text=EAPPX%20file%20is%20a%20Microsoft%20Windows%20Encrypted%20App,applications%20may%20also%20use%20the%20.eappx%20file%20extension."));
-        });
-
-        public IAsyncRelayCommand BlockMapCommand { get; set; } = new AsyncRelayCommand(async () =>
-        {
-            await Windows.System.Launcher.LaunchUriAsync(new Uri("https://docs.microsoft.com/en-us/uwp/schemas/blockmapschema/app-package-block-map"));
-        });
-
-        public LinkFilterViewModel(ILinkFilterService linkFilterService)
+        public LinkFilterViewModel(ILinkFilterService linkFilterService,INavigationService navigationService)
         {
             LinkFilterService = linkFilterService;
+            NavigationService = navigationService;
 
             StartsWithEFilterValue = LinkFilterService.StartWithEFilterValue;
             BlockMapFilterValue = LinkFilterService.BlockMapFilterValue;
+
+            LinkFilterInstructionCommand = new AsyncRelayCommand(async () =>
+            {
+                NavigationService.NavigateTo(typeof(AboutViewModel).FullName, null, new DrillInNavigationTransitionInfo());
+                await Task.CompletedTask;
+            });
 
             StartWithEFilterCommand = new AsyncRelayCommand<bool>(async (param) =>
             {
