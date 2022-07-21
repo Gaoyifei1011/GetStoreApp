27d6b174004273c73e737f447524fbd72cac32a4
高怡飞 <49179966+Gaoyifei1011@users.noreply.github.com>
2022-07-22 00:23:18 +0800

    Update download settings
    Update download settings

---
+++ Services/Settings/DownloadOptionsService.cs
@@ -0,0 +1,8 @@
+﻿using GetStoreApp.Contracts.Services.Settings;
+
+namespace GetStoreApp.Services.Settings
+{
+    public class DownloadOptionsService : IDownloadOptionsService
+    {
+    }
+}
--- UI/Controls/Settings/DownloadOptionsControl.xaml.cs
+++ UI/Controls/Settings/DownloadOptionsControl.xaml.cs
@@ -1,8 +1,7 @@
 ﻿using GetStoreApp.Contracts.Services.App;
+using GetStoreApp.ViewModels.Controls.Settings;
 using Microsoft.UI.Xaml.Controls;
-
-// To learn more about WinUI, the WinUI project structure,
-// and more about our project templates, see: http://aka.ms/winui-project-info.
+using Windows.Storage;
 
 namespace GetStoreApp.UI.Controls.Settings
 {
@@ -10,10 +9,18 @@ namespace GetStoreApp.UI.Controls.Settings
     {
         public IResourceService ResourceService { get; }
 
+        public DownloadOptionsViewModel ViewModel { get; }
+
         public DownloadOptionsControl()
         {
             ResourceService = App.GetService<IResourceService>();
+            ViewModel = App.GetService<DownloadOptionsViewModel>();
             this.InitializeComponent();
+        }
+
+        public string DownloadFolderPath(StorageFolder folder)
+        {
+            return folder.Path;
         }
     }
 }
