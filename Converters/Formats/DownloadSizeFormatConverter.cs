9001eae70469d2cccd4ab0fa116fa446cc439f69
高怡飞 <49179966+Gaoyifei1011@users.noreply.github.com>
2022-07-29 00:07:00 +0800

    Add download speed and size information
    Add download speed and size information

---
+++ Converters/Formats/DownloadSizeFormatConverter.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Data;
+using System;
+using System.Collections.Generic;
+
+namespace GetStoreApp.Converters.Formats
+{
+    /// <summary>
+    /// 下载文件大小文字显示转换器
+    /// </summary>
+    public class DownloadSizeFormatConverter : IValueConverter
+    {
+        public List<string> SizeUnitsList = new List<string>() { "KB", "MB", "GB" };
+
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (value == null)
+                return DependencyProperty.UnsetValue;
+
+            int? result = value as int?;
+
+            return string.Format("{0}{1}", result, SizeUnitsList[1]);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- ViewModels/Controls/Home/ResultViewModel.cs
+++ ViewModels/Controls/Home/ResultViewModel.cs
@@ -8,6 +8,7 @@ using GetStoreApp.UI.Dialogs;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -115,6 +116,7 @@ namespace GetStoreApp.ViewModels.Controls.Home
 
             FileOperationCommand = new AsyncRelayCommand<string>(async (param) =>
             {
+                Debug.WriteLine("hello");
                 await FileOperationAsync(param);
             });
 
