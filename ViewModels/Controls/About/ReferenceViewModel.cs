69f3196335d8fff145ec3d94684a308c7492b322
高怡飞 <49179966+Gaoyifei1011@users.noreply.github.com>
2022-08-20 00:21:56 +0800

    Add en-us and zh-cn readme.md
    Add en-us and zh-cn readme.md

--- ViewModels/Controls/About/ReferenceViewModel.cs
+++ ViewModels/Controls/About/ReferenceViewModel.cs
@@ -7,17 +7,17 @@ namespace GetStoreApp.ViewModels.Controls.About
     {
         public Dictionary<string, string> ReferenceDict { get; } = new Dictionary<string, string>
         {
-            {"Aira2","https://aria2.github.io/" },
+            {"Aira2","https://aria2.github.io" },
             {"CommunityToolkit.Mvvm","https://github.com/CommunityToolkit/WindowsCommunityToolkit" },
             {"CommunityToolkit.WinUI.UI.Controls.DataGrid" ,"https://docs.microsoft.com/en-us/windows/communitytoolkit/controls/datagrid"},
-            {"CommunityToolkit.WinUI.Notifications","https://www.nuget.org/packages/CommunityToolkit.WinUI.Notifications/" },
-            {"HtmlAgilityPack","http://html-agility-pack.net/" },
-            {"Microsoft.Data.Sqlite","https://docs.microsoft.com/dotnet/standard/data/sqlite/" },
-            {"Microsoft.Extensions.Hosting","https://www.nuget.org/packages/Microsoft.Extensions.Hosting/" },
+            {"CommunityToolkit.WinUI.Notifications","https://www.nuget.org/packages/CommunityToolkit.WinUI.Notifications" },
+            {"HtmlAgilityPack","http://html-agility-pack.net" },
+            {"Microsoft.Data.Sqlite","https://docs.microsoft.com/dotnet/standard/data/sqlite" },
+            {"Microsoft.Extensions.Hosting","https://www.nuget.org/packages/Microsoft.Extensions.Hosting" },
             {"Microsoft.Windows.CsWin32","https://github.com/Microsoft/CsWin32" },
             {"Microsoft.Windows.SDK.BuildTools","https://www.nuget.org/packages/Microsoft.Windows.SDK.BuildTools" },
             {"Microsoft.WindowsAppSDK","https://github.com/microsoft/windowsappsdk" },
-            {"Microsoft.Xaml.Behaviors.WinUI.Managed","https://www.nuget.org/packages/Microsoft.Xaml.Behaviors.WinUI.Managed/" },
+            {"Microsoft.Xaml.Behaviors.WinUI.Managed","https://www.nuget.org/packages/Microsoft.Xaml.Behaviors.WinUI.Managed" },
             {"NETStandard.Library","https://www.nuget.org/packages/NETStandard.Library" },
             {"Newtonsoft.Json" ,"https://www.newtonsoft.com/json"},
             {"PInvoke.SHCore","https://github.com/dotnet/pinvoke" },
