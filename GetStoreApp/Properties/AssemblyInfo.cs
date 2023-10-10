985e13a41ebb28fbe670b8fd9fd5be757bae3976
高怡飞 <363301617@qq.com>
2023-10-10 21:21:32 +0800

    Bring back Flyoutbase SystemBackdrop
    Bring back Flyoutbase SystemBackdrop

--- GetStoreApp/Properties/AssemblyInfo.cs
+++ GetStoreApp/Properties/AssemblyInfo.cs
@@ -4,15 +4,16 @@ using System.Runtime.InteropServices;
 using System.Runtime.Versioning;
 
 // 程序集信息设置
+[assembly: AssemblyCompany("高怡飞")]
 [assembly: AssemblyCopyright("Copyright ©2022-2023 高怡飞, All Rights Reserved.")]
 [assembly: AssemblyDescription("获取商店应用")]
-[assembly: AssemblyFileVersion("2.4.1009.0")]
-[assembly: AssemblyInformationalVersion("2.4.1009.0")]
+[assembly: AssemblyFileVersion("2.4.1010.0")]
+[assembly: AssemblyInformationalVersion("2.4.1010.0")]
 [assembly: AssemblyProduct("获取商店应用")]
 [assembly: AssemblyTitle("获取商店应用")]
-[assembly: AssemblyVersion("2.4.1009.0")]
+[assembly: AssemblyVersion("2.4.1010.0")]
 
-// 设置程序集对COM组件的访问权限
+// 设置程序集对 COM 组件的访问权限
 [assembly: ComVisible(false)]
 
 // 如果此项目向 COM 公开，则下列 GUID 用于类型库的 ID
