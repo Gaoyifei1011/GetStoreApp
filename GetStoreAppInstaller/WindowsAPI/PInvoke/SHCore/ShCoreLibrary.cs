b7dca2863f3b805cfc418204834e363227c7a4ca
高怡飞 <363301617@qq.com>
2025-01-16 22:28:53 +0800

    Import CreateStreamOverRandomAccessStream winrt api
    Import CreateStreamOverRandomAccessStream winrt api

--- GetStoreAppInstaller/WindowsAPI/PInvoke/SHCore/ShCoreLibrary.cs
+++ GetStoreAppInstaller/WindowsAPI/PInvoke/SHCore/ShCoreLibrary.cs
@@ -17,6 +17,16 @@ namespace GetStoreAppInstaller.WindowsAPI.PInvoke.SHCore
         /// <param name="ppv">此方法成功返回时，包含指向 RandomAccessStream 的接口指针，该指针封装 riid 中请求的流。</param>
         /// <returns>如果此函数成功，则返回 S_OK。 否则，将返回 HRESULT 错误代码。</returns>
         [LibraryImport(ShCore, EntryPoint = "CreateRandomAccessStreamOverStream", SetLastError = false), PreserveSig]
-        public static partial int CreateRandomAccessStreamOverStream(IStream stream, BSOS_OPTIONS options, Guid riid, out IntPtr ppv);
+        public static partial int CreateRandomAccessStreamOverStream([MarshalAs(UnmanagedType.Interface)] IStream stream, BSOS_OPTIONS options, Guid riid, out IntPtr ppv);
+
+        /// <summary>
+        /// 围绕 Windows 运行时 IRandomAccessStream 对象创建 IStream。
+        /// </summary>
+        /// <param name="randomAccessStream">源 IRandomAccessStream。</param>
+        /// <param name="riid">对要通过 ppv 检索的接口的 IID 的引用，通常IID_IStream。 此对象封装 randomAccessStream。</param>
+        /// <param name="ppv">此方法成功返回时，包含 riid 中请求的接口指针，通常为 IStream。</param>
+        /// <returns></returns>
+        [LibraryImport(ShCore, EntryPoint = "CreateStreamOverRandomAccessStream", SetLastError = false), PreserveSig]
+        public static partial int CreateStreamOverRandomAccessStream(IntPtr randomAccessStream, Guid riid, [MarshalAs(UnmanagedType.Interface)] out IStream ppv);
     }
 }
