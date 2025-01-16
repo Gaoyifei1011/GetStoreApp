89fb80718e5acf7a76262a6c8d31af651e88cb11
高怡飞 <363301617@qq.com>
2025-01-16 22:26:12 +0800

    Add ToastNotificationService
    Add ToastNotificationService

---
+++ GetStoreAppInstaller/Services/Root/ToastNotificationService.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Windows.AppNotifications;
+using System;
+using Windows.Data.Xml.Dom;
+using Windows.Foundation.Diagnostics;
+using Windows.UI.Notifications;
+
+namespace GetStoreAppInstaller.Services.Root
+{
+    /// <summary>
+    /// 应用通知服务
+    /// </summary>
+    public class ToastNotificationService
+    {
+        public static ToastNotifier AppToastNotifier { get; } = ToastNotificationManager.CreateToastNotifier();
+
+        /// <summary>
+        /// 显示通知
+        /// </summary>
+        public static void Show(AppNotification appNotification)
+        {
+            try
+            {
+                XmlDocument notificationDocument = new();
+                notificationDocument.LoadXml(appNotification.Payload);
+                ToastNotification notificaiton = new(notificationDocument);
+                AppToastNotifier.Show(notificaiton);
+            }
+            catch (Exception e)
+            {
+                LogService.WriteLog(LoggingLevel.Error, "Show notification failed", e);
+            }
+        }
+    }
+}
