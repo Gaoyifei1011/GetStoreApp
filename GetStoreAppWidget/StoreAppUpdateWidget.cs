a31f8696780c993ef5c39a8b5d8ee6ea6f0a5fbc
高怡飞 <363301617@qq.com>
2025-01-31 16:04:12 +0800

    Remove GetStoreAppWidget
    Remove GetStoreAppWidget

--- GetStoreAppWidget/StoreAppUpdateWidget.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace GetStoreAppWidget
-{
-    /// <summary>
-    /// 商店应用更新小组件
-    /// </summary>
-    public class StoreAppUpdateWidget
-    {
-    }
-}
--- GetStoreAppWidget/Win32AppUpdateWidget.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace GetStoreAppWidget
-{
-    /// <summary>
-    /// Win32 传统应用更新小组件
-    /// </summary>
-    public class Win32AppUpdateWidget
-    {
-    }
-}
