1ead0f3cc6bfe99206142415ce6b654aa1900de1
高怡飞 <363301617@qq.com>
2025-02-12 13:04:52 +0800

    Import IOverlappedPresenter3
    Import IOverlappedPresenter3

---
+++ GetStoreAppInstaller/WindowsAPI/ComTypes/IOverlappedPresenter3.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Runtime.InteropServices.Marshalling;
+using Windows.Graphics;
+
+namespace GetStoreAppInstaller.WindowsAPI.ComTypes
+{
+    [GeneratedComInterface, Guid("FF7841F0-8253-5ADF-ABBA-57919BFE1CAC")]
+    public partial interface IOverlappedPresenter3 : IInspectable
+    {
+        [PreserveSig]
+        public int GetPreferredMaximumSize(out SizeInt32 size);
+
+        [PreserveSig]
+        public void SetPreferredMaximumSize(SizeInt32 size);
+
+        [PreserveSig]
+        public int GetPreferredMinimumSize(out SizeInt32 size);
+
+        [PreserveSig]
+        public void SetPreferredMinimumSize(SizeInt32 size);
+
+        [PreserveSig]
+        public void SetPreferredBounds(SizeInt32 preferredMinimumSize, SizeInt32 preferredMaximumSize);
+    }
+}
