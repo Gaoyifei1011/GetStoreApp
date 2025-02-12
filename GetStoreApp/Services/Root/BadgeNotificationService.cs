28ecb05df2e79ba4017063a8d0a1bbbbc2bef622
高怡飞 <363301617@qq.com>
2025-02-12 13:06:42 +0800

    Import BadgeNotificationManager API
    Import BadgeNotificationManager API

--- GetStoreApp/Services/Root/BadgeNotificationService.cs
+++ GetStoreApp/Services/Root/BadgeNotificationService.cs
@@ -1,25 +1,20 @@
-﻿using Windows.Data.Xml.Dom;
-using Windows.UI.Notifications;
+﻿using Microsoft.Windows.BadgeNotifications;
 
 namespace GetStoreApp.Services.Root
 {
     /// <summary>
-    /// 应用任务栏数字角标提示服务
+    /// 应用任务栏角标通知提示服务
     /// </summary>
     public static class BadgeNotificationService
     {
-        private static readonly string badgeXmlString = "<badge value='{0}'/>";
-        private static readonly BadgeUpdater badgeUpdater = BadgeUpdateManager.CreateBadgeUpdaterForApplication();
-        private static readonly XmlDocument badgeDocument = new();
+        private static readonly BadgeNotificationManager badgeNotificationManager = BadgeNotificationManager.Current;
 
         /// <summary>
         /// 设置任务栏数字角标的值，并显示
         /// </summary>
         public static void Show(int value)
         {
-            badgeDocument.LoadXml(string.Format(badgeXmlString, value));
-            BadgeNotification badge = new(badgeDocument);
-            badgeUpdater.Update(badge);
+            badgeNotificationManager.SetBadgeAsCount((uint)value);
         }
     }
 }
