8eba991268c948ae9d37881e9f8f61a906d053f9
高怡飞 <363301617@qq.com>
2025-04-10 12:43:38 +0800

    Add IContentIslandPartner COM Interface
    Add IContentIslandPartner COM Interface

---
+++ GetStoreApp/WindowsAPI/ComTypes/IContentIslandPartner.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Runtime.InteropServices.Marshalling;
+
+namespace GetStoreApp.WindowsAPI.ComTypes
+{
+    [GeneratedComInterface, Guid("70CE8275-8D05-5222-A00B-41F9F9B2150C")]
+    public partial interface IContentIslandPartner : IInspectable
+    {
+        [PreserveSig]
+        int GetIslandInputSite(out IntPtr islandInputSitePartner);
+
+        [PreserveSig]
+        int Get_TEMP_DesktopSiteBridge(out IntPtr desktopSiteBridge);
+    }
+}
