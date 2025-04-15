606cda55fdfd8ffc5cf9c4601b106c20780c571a
高怡飞 <363301617@qq.com>
2025-04-15 14:34:38 +0800

    Fixed the issue that the WinGet page was slow to load for the first time
    Fixed the issue that the WinGet page was slow to load for the first time

--- GetStoreApp/Views/Pages/WinGetPage.xaml.cs
+++ GetStoreApp/Views/Pages/WinGetPage.xaml.cs
@@ -1,15 +1,12 @@
 using GetStoreApp.Models.Controls.WinGet;
 using GetStoreApp.Services.Controls.Settings;
 using GetStoreApp.Services.Root;
-using GetStoreApp.WindowsAPI.ComTypes;
-using GetStoreApp.WindowsAPI.PInvoke.Ole32;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Input;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.Runtime.InteropServices;
 using System.Threading;
 using System.Threading.Tasks;
 using Windows.Foundation;
@@ -27,9 +24,6 @@ namespace GetStoreApp.Views.Pages
     public sealed partial class WinGetPage : Page
     {
         private bool isInitialized;
-        private readonly Guid CLSID_OpenControlPanel = new("06622D85-6856-4460-8DE1-A81921B41C4B");
-        private IOpenControlPanel openControlPanel;
-
         public readonly Lock installStateLock = new();
 
         internal Dictionary<string, IAsyncInfo> InstallingStateDict { get; } = [];
@@ -39,16 +33,6 @@ namespace GetStoreApp.Views.Pages
         public WinGetPage()
         {
             InitializeComponent();
-
-            Task.Run(() =>
-            {
-                int createResult = Ole32Library.CoCreateInstance(CLSID_OpenControlPanel, IntPtr.Zero, CLSCTX.CLSCTX_INPROC_SERVER | CLSCTX.CLSCTX_INPROC_HANDLER | CLSCTX.CLSCTX_LOCAL_SERVER | CLSCTX.CLSCTX_REMOTE_SERVER, typeof(IOpenControlPanel).GUID, out IntPtr ppv);
-
-                if (createResult is 0)
-                {
-                    openControlPanel = (IOpenControlPanel)Program.StrategyBasedComWrappers.GetOrCreateObjectForComInstance(ppv, CreateObjectFlags.Unwrap);
-                }
-            });
         }
 
         #region 第一部分：XamlUICommand 命令调用时挂载的事件
@@ -100,39 +84,6 @@ namespace GetStoreApp.Views.Pages
         #region 第二部分：WinGet 程序包页面——挂载的事件
 
         /// <summary>
-        /// 分割控件选中项发生改变时引发的事件
-        /// </summary>
-        private void OnSelectionChanged(object sender, SelectorBarSelectionChangedEventArgs args)
-        {
-            if (sender is SelectorBar selectorBar && selectorBar.SelectedItem is not null)
-            {
-                int selectedIndex = selectorBar.Items.IndexOf(selectorBar.SelectedItem);
-
-                if (selectedIndex is 0)
-                {
-                    SearchApps.Visibility = Visibility.Visible;
-                    InstalledApps.Visibility = Visibility.Collapsed;
-                    UpgradableApps.Visibility = Visibility.Collapsed;
-                    ControlPanel.Visibility = Visibility.Collapsed;
-                }
-                else if (selectedIndex is 1)
-                {
-                    SearchApps.Visibility = Visibility.Collapsed;
-                    InstalledApps.Visibility = Visibility.Visible;
-                    UpgradableApps.Visibility = Visibility.Collapsed;
-                    ControlPanel.Visibility = Visibility.Visible;
-                }
-                else if (selectedIndex is 2)
-                {
-                    SearchApps.Visibility = Visibility.Collapsed;
-                    InstalledApps.Visibility = Visibility.Collapsed;
-                    UpgradableApps.Visibility = Visibility.Visible;
-                    ControlPanel.Visibility = Visibility.Collapsed;
-                }
-            }
-        }
-
-        /// <summary>
         /// 初始化 WinGet 程序包页面
         /// </summary>
         private void OnLoaded(object sender, RoutedEventArgs args)
@@ -163,17 +114,6 @@ namespace GetStoreApp.Views.Pages
         }
 
         /// <summary>
-        /// 打开控制面板的程序与功能
-        /// </summary>
-        private void OnControlPanelClicked(object sender, RoutedEventArgs args)
-        {
-            Task.Run(() =>
-            {
-                openControlPanel?.Open("Microsoft.ProgramsAndFeatures", null, IntPtr.Zero);
-            });
-        }
-
-        /// <summary>
         /// 了解更多有关 WinGet 程序包的描述信息
         /// </summary>
         private async void OnLearnMoreClicked(object sender, RoutedEventArgs args)
@@ -198,5 +138,10 @@ namespace GetStoreApp.Views.Pages
         }
 
         #endregion 第二部分：WinGet 程序包页面——挂载的事件
+
+        private Visibility GetSelectedItem(SelectorBarItem selectorBarItem, int index)
+        {
+            return WinGetSelectorBar is not null ? WinGetSelectorBar.Items.IndexOf(selectorBarItem).Equals(index) ? Visibility.Visible : Visibility.Collapsed : Visibility.Collapsed;
+        }
     }
 }
