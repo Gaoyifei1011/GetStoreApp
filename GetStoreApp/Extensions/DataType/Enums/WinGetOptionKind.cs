dac34a3a097e303e6758bcc1317243c5b22075bc
高怡飞 <363301617@qq.com>
2025-05-04 13:44:19 +0800

    Update WinGetOptionKind.cs

--- GetStoreApp/Extensions/DataType/Enums/WinGetOptionKind.cs
+++ GetStoreApp/Extensions/DataType/Enums/WinGetOptionKind.cs
@@ -1,13 +1,21 @@
 ﻿namespace GetStoreApp.Extensions.DataType.Enums
 {
     /// <summary>
-    /// WinGet 复制命令枚举值
+    /// WinGet 操作枚举值
     /// </summary>
     public enum WinGetOptionKind
     {
-        SearchInstall = 0,
-        Uninstall = 1,
-        Repair = 2,
-        UpgradeInstall = 3
+        SearchDownloadCopy = 0,
+        SearchInstallCopy = 1,
+        SearchRepairCopy = 2,
+        SearchDownload = 3,
+        SearchInstall = 4,
+        SearchRepair = 5,
+        SearchDownloadCommand = 6,
+        SearchInstallCommand = 7,
+        SearchRepairCommand = 8,
+        Uninstall = 9,
+        Upgrade = 10,
+        VersionInfo = 11
     }
 }
