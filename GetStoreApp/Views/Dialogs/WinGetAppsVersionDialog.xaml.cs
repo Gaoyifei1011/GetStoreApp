f4afe2f78f8c9ab65e114564b30d35e466f4eab8
高怡飞 <363301617@qq.com>
2025-05-04 13:37:01 +0800

    Add WinGetAppsVersionDialog
    Add WinGetAppsVersionDialog

---
+++ GetStoreApp/Views/Dialogs/WinGetAppsVersionDialog.xaml.cs
@@ -0,0 +1,24 @@
+using GetStoreApp.Extensions.DataType.Enums;
+using GetStoreApp.Models.Controls.WinGet;
+using GetStoreApp.Views.Pages;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+
+namespace GetStoreApp.Views.Dialogs
+{
+    public sealed partial class WinGetAppsVersionDialog : ContentDialog
+    {
+        public WinGetAppsVersionDialog(WinGetOptionKind winGetOptionKind, WinGetPage winGetPage, SearchAppsModel searchApps)
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// ¹Ø±Õ¶Ô»°¿ò
+        /// </summary>
+        private void OnCloseClicked(object sender, RoutedEventArgs args)
+        {
+            Hide();
+        }
+    }
+}
