a2c9efb4c26b63459967d5acdd1e590bd5c5bbbe
高怡飞 <363301617@qq.com>
2025-05-10 22:36:45 +0800

    Add missing navigation implementation
    Add missing navigation implementation

--- GetStoreApp/Views/Windows/MainWindow.xaml.cs
+++ GetStoreApp/Views/Windows/MainWindow.xaml.cs
@@ -477,7 +477,11 @@ namespace GetStoreApp.Views.Windows
             {
                 if (GetFrameContent() is AppManagerPage appManagerPage && appManagerPage.BreadCollection.Count is 2)
                 {
-                    appManagerPage.NavigateTo(typeof(AppListPage), null, false);
+                    appManagerPage.NavigateTo(appManagerPage.PageList[0], null, false);
+                }
+                else if (GetFrameContent() is SettingsPage settingsPage && settingsPage.BreadCollection.Count is 2)
+                {
+                    settingsPage.NavigateTo(settingsPage.PageList[0], null, false);
                 }
                 else
                 {
