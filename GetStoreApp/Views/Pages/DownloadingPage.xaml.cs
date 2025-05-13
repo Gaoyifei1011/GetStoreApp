01dc2d8d7eb3d96aa699ebff0367aadd25096b92
高怡飞 <363301617@qq.com>
2025-05-14 01:26:33 +0800

    Add DownloadingPage
    Add DownloadingPage

---
+++ GetStoreApp/Views/Pages/DownloadingPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.UI.Xaml.Controls;
+
+namespace GetStoreApp.Views.Pages
+{
+    /// <summary>
+    /// 下载中页面
+    /// </summary>
+    public sealed partial class DownloadingPage : Page
+    {
+        public DownloadingPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
