58f73a40328ad8542051b0cd201621af1b3ce1be
高怡飞 <363301617@qq.com>
2025-05-14 01:26:22 +0800

    Add CompletedPage
    Add CompletedPage

---
+++ GetStoreApp/Views/Pages/CompletedPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.UI.Xaml.Controls;
+
+namespace GetStoreApp.Views.Pages
+{
+    /// <summary>
+    /// 下载已完成页面
+    /// </summary>
+    public sealed partial class CompletedPage : Page
+    {
+        public CompletedPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
