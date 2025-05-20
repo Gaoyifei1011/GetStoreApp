31496e2f98942b30ce4a737ee3f030a8368ab6da
高怡飞 <363301617@qq.com>
2025-05-20 22:06:07 +0800

    Add DownloadProgress
    Add DownloadProgress

---
+++ GetStoreApp/Extensions/DataType/Classes/DownloadProgress.cs
@@ -0,0 +1,46 @@
+﻿using GetStoreApp.Extensions.DataType.Enums;
+using System;
+
+namespace GetStoreApp.Extensions.DataType.Classes
+{
+    /// <summary>
+    /// 下载进度
+    /// </summary>
+    public class DownloadProgress
+    {
+        /// <summary>
+        /// 下载对应的 ID
+        /// </summary>
+        public string DownloadID { get; set; }
+
+        /// <summary>
+        /// 下载文件名称
+        /// </summary>
+        public string FileName { get; set; }
+
+        /// <summary>
+        /// 下载文件路径
+        /// </summary>
+        public string FilePath { get; set; }
+
+        /// <summary>
+        /// 下载文件状态
+        /// </summary>
+        public DownloadProgressState DownloadProgressState { get; set; }
+
+        /// <summary>
+        /// 已下载完成的大小
+        /// </summary>
+        public double CompletedSize { get; set; }
+
+        /// <summary>
+        /// 文件总大小
+        /// </summary>
+        public double TotalSize { get; set; }
+
+        /// <summary>
+        /// 文件下载速度
+        /// </summary>
+        public double DownloadSpeed { get; set; }
+    }
+}
---
+++ GetStoreApp/Extensions/DataType/Enums/DownloadProgressState.cs
@@ -0,0 +1,15 @@
+﻿namespace GetStoreApp.Extensions.DataType.Enums
+{
+    /// <summary>
+    /// 下载进度状态
+    /// </summary>
+    public enum DownloadProgressState
+    {
+        Queued = 0,
+        Downloading = 1,
+        Paused = 2,
+        Failed = 3,
+        Finished = 4,
+        Deleted = 5,
+    }
+}
