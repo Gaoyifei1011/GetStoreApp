7f0a362f3a50d3bbe4449d5ae378b98c084bc412
高怡飞 <363301617@qq.com>
2025-05-20 22:07:56 +0800

    Update BitsService implementation
    Update BitsService implementation

--- GetStoreApp/Services/Controls/Download/BitsService.cs
+++ GetStoreApp/Services/Controls/Download/BitsService.cs
@@ -1,4 +1,6 @@
-﻿using GetStoreApp.Services.Root;
+﻿using GetStoreApp.Extensions.DataType.Classes;
+using GetStoreApp.Extensions.DataType.Enums;
+using GetStoreApp.Services.Root;
 using GetStoreApp.WindowsAPI.ComTypes;
 using GetStoreApp.WindowsAPI.PInvoke.Ole32;
 using System;
@@ -23,19 +25,9 @@ namespace GetStoreApp.Services.Controls.Download
 
         private static IBackgroundCopyManager backgroundCopyManager;
 
-        private static Dictionary<Guid, (IBackgroundCopyJob backgroundCopyJob, BackgroundCopyCallback backgroundCopyCallback)> BitsDict { get; } = [];
+        private static Dictionary<string, (string saveFilePath, IBackgroundCopyJob backgroundCopyJob, BackgroundCopyCallback backgroundCopyCallback)> BitsDict { get; } = [];
 
-        public static event Action<Guid, string, string, string, double> DownloadCreated;
-
-        public static event Action<Guid> DownloadContinued;
-
-        public static event Action<Guid> DownloadPaused;
-
-        public static event Action<Guid> DownloadDeleted;
-
-        public static event Action<Guid, BG_JOB_PROGRESS> DownloadProgressing;
-
-        public static event Action<Guid, BG_JOB_PROGRESS> DownloadCompleted;
+        public static event Action<DownloadProgress> DownloadProgress;
 
         /// <summary>
         /// 初始化后台智能传输服务
@@ -64,30 +56,6 @@ namespace GetStoreApp.Services.Controls.Download
         }
 
         /// <summary>
-        /// 获取下载任务的数量
-        /// </summary>
-        public static int GetDownloadCount()
-        {
-            int count = 0;
-            bitsLock.Enter();
-
-            try
-            {
-                count = BitsDict.Count;
-            }
-            catch (Exception e)
-            {
-                ExceptionAsVoidMarshaller.ConvertToUnmanaged(e);
-            }
-            finally
-            {
-                bitsLock.Exit();
-            }
-
-            return count;
-        }
-
-        /// <summary>
         /// 终止所有下载任务，仅用于应用关闭时
         /// </summary>
         public static void TerminateDownload()
@@ -96,25 +64,22 @@ namespace GetStoreApp.Services.Controls.Download
             {
                 try
                 {
-                    if (GetDownloadCount() > 0)
-                    {
-                        bitsLock.Enter();
+                    bitsLock.Enter();
 
-                        try
+                    try
+                    {
+                        foreach (KeyValuePair<string, (string saveFilePath, IBackgroundCopyJob backgroundCopyJob, BackgroundCopyCallback backgroundCopyCallback)> bits in BitsDict)
                         {
-                            foreach (KeyValuePair<Guid, (IBackgroundCopyJob backgroundCopyJob, BackgroundCopyCallback backgroundCopyCallback)> bitsKeyValue in BitsDict)
-                            {
-                                bitsKeyValue.Value.backgroundCopyJob.Cancel();
-                            }
+                            bits.Value.backgroundCopyJob.Cancel();
                         }
-                        catch (Exception e)
-                        {
-                            LogService.WriteLog(LoggingLevel.Error, "Terminate all task failed", e);
-                        }
-                        finally
-                        {
-                            bitsLock.Exit();
-                        }
+                    }
+                    catch (Exception e)
+                    {
+                        LogService.WriteLog(LoggingLevel.Error, "Terminate all task failed", e);
+                    }
+                    finally
+                    {
+                        bitsLock.Exit();
                     }
                 }
                 catch (Exception e)
@@ -146,13 +111,12 @@ namespace GetStoreApp.Services.Controls.Download
                         downloadJob.SetNotifyInterface(Program.StrategyBasedComWrappers.GetOrCreateComInterfaceForObject(new UnknownWrapper(backgroundCopyCallback).WrappedObject, CreateComInterfaceFlags.None));
 
                         downloadJob.GetProgress(out BG_JOB_PROGRESS progress);
-                        DownloadCreated?.Invoke(backgroundCopyCallback.DownloadID, Path.GetFileName(saveFilePath), saveFilePath, url, progress.BytesTotal is ulong.MaxValue ? 0 : progress.BytesTotal);
 
                         bitsLock.Enter();
 
                         try
                         {
-                            BitsDict.TryAdd(downloadID, ValueTuple.Create(downloadJob, backgroundCopyCallback));
+                            BitsDict.TryAdd(downloadID.ToString(), ValueTuple.Create(saveFilePath, downloadJob, backgroundCopyCallback));
                         }
                         catch (Exception e)
                         {
@@ -162,6 +126,17 @@ namespace GetStoreApp.Services.Controls.Download
                         {
                             bitsLock.Exit();
                         }
+
+                        DownloadProgress?.Invoke(new DownloadProgress()
+                        {
+                            DownloadID = downloadID.ToString(),
+                            DownloadProgressState = DownloadProgressState.Queued,
+                            FileName = Path.GetFileName(saveFilePath),
+                            FilePath = saveFilePath,
+                            DownloadSpeed = 0,
+                            CompletedSize = 0,
+                            TotalSize = 0,
+                        });
 
                         downloadJob.Resume();
                     }
@@ -176,7 +151,7 @@ namespace GetStoreApp.Services.Controls.Download
         /// <summary>
         /// 继续下载
         /// </summary>
-        public static void ContinueDownload(Guid downloadID)
+        public static void ContinueDownload(string downloadID)
         {
             Task.Factory.StartNew((param) =>
             {
@@ -184,13 +159,22 @@ namespace GetStoreApp.Services.Controls.Download
 
                 try
                 {
-                    if (BitsDict.TryGetValue(downloadID, out (IBackgroundCopyJob backgroundCopyJob, BackgroundCopyCallback backgroundCopyCallback) downloadValue))
+                    if (BitsDict.TryGetValue(downloadID, out (string saveFilePath, IBackgroundCopyJob backgroundCopyJob, BackgroundCopyCallback backgroundCopyCallback) downloadValue))
                     {
                         int continueResult = downloadValue.backgroundCopyJob.Resume();
 
                         if (continueResult is 0)
                         {
-                            DownloadContinued?.Invoke(downloadID);
+                            DownloadProgress?.Invoke(new DownloadProgress()
+                            {
+                                DownloadID = downloadID,
+                                DownloadProgressState = DownloadProgressState.Queued,
+                                FileName = Path.GetFileName(downloadValue.saveFilePath),
+                                FilePath = downloadValue.saveFilePath,
+                                DownloadSpeed = 0,
+                                CompletedSize = 0,
+                                TotalSize = 0,
+                            });
                         }
                     }
                 }
@@ -208,7 +192,7 @@ namespace GetStoreApp.Services.Controls.Download
         /// <summary>
         /// 暂停下载
         /// </summary>
-        public static void PauseDownload(Guid downloadID)
+        public static void PauseDownload(string downloadID)
         {
             Task.Factory.StartNew((param) =>
             {
@@ -216,13 +200,22 @@ namespace GetStoreApp.Services.Controls.Download
 
                 try
                 {
-                    if (BitsDict.TryGetValue(downloadID, out (IBackgroundCopyJob backgroundCopyJob, BackgroundCopyCallback backgroundCopyCallback) downloadValue))
+                    if (BitsDict.TryGetValue(downloadID, out (string saveFilePath, IBackgroundCopyJob backgroundCopyJob, BackgroundCopyCallback backgroundCopyCallback) downloadValue))
                     {
                         int pauseResult = downloadValue.backgroundCopyJob.Suspend();
 
                         if (pauseResult is 0)
                         {
-                            DownloadPaused?.Invoke(downloadID);
+                            DownloadProgress?.Invoke(new DownloadProgress()
+                            {
+                                DownloadID = downloadID,
+                                DownloadProgressState = DownloadProgressState.Paused,
+                                FileName = Path.GetFileName(downloadValue.saveFilePath),
+                                FilePath = downloadValue.saveFilePath,
+                                DownloadSpeed = 0,
+                                CompletedSize = 0,
+                                TotalSize = 0,
+                            });
                         }
                     }
                 }
@@ -240,7 +233,7 @@ namespace GetStoreApp.Services.Controls.Download
         /// <summary>
         /// 删除下载
         /// </summary>
-        public static void DeleteDownload(Guid downloadID)
+        public static void DeleteDownload(string downloadID)
         {
             Task.Factory.StartNew((param) =>
             {
@@ -248,15 +241,24 @@ namespace GetStoreApp.Services.Controls.Download
 
                 try
                 {
-                    if (BitsDict.TryGetValue(downloadID, out (IBackgroundCopyJob backgroundCopyJob, BackgroundCopyCallback backgroundCopyCallback) downloadValue))
+                    if (BitsDict.TryGetValue(downloadID, out (string saveFilePath, IBackgroundCopyJob backgroundCopyJob, BackgroundCopyCallback backgroundCopyCallback) downloadValue))
                     {
                         int deleteResult = downloadValue.backgroundCopyJob.Cancel();
 
                         if (deleteResult is 0)
                         {
                             downloadValue.backgroundCopyCallback.StatusChanged -= OnStatusChanged;
-                            DownloadDeleted?.Invoke(downloadID);
                             BitsDict.Remove(downloadID);
+                            DownloadProgress?.Invoke(new DownloadProgress()
+                            {
+                                DownloadID = downloadID,
+                                DownloadProgressState = DownloadProgressState.Deleted,
+                                FileName = Path.GetFileName(downloadValue.saveFilePath),
+                                FilePath = downloadValue.saveFilePath,
+                                DownloadSpeed = 0,
+                                CompletedSize = 0,
+                                TotalSize = 0,
+                            });
                         }
                     }
                 }
@@ -276,15 +278,29 @@ namespace GetStoreApp.Services.Controls.Download
         /// </summary>
         private static void OnStatusChanged(BackgroundCopyCallback callback, IBackgroundCopyJob downloadJob, BG_JOB_STATE state)
         {
+            // 下载文件中
             if (state is BG_JOB_STATE.BG_JOB_STATE_TRANSFERRING)
             {
                 downloadJob.GetProgress(out BG_JOB_PROGRESS progress);
-                DownloadProgressing?.Invoke(callback.DownloadID, progress);
+
+                if (BitsDict.TryGetValue(callback.DownloadID.ToString(), out (string saveFilePath, IBackgroundCopyJob backgroundCopyJob, BackgroundCopyCallback backgroundCopyCallback) downloadValue))
+                {
+                    DownloadProgress?.Invoke(new DownloadProgress()
+                    {
+                        DownloadID = callback.DownloadID.ToString(),
+                        DownloadProgressState = DownloadProgressState.Downloading,
+                        FileName = Path.GetFileName(downloadValue.saveFilePath),
+                        FilePath = downloadValue.saveFilePath,
+                        DownloadSpeed = 0,
+                        CompletedSize = progress.BytesTransferred,
+                        TotalSize = progress.BytesTotal,
+                    });
+                }
             }
+            // 下载完成
             else if (state is BG_JOB_STATE.BG_JOB_STATE_TRANSFERRED)
             {
                 downloadJob.GetProgress(out BG_JOB_PROGRESS progress);
-                DownloadCompleted?.Invoke(callback.DownloadID, progress);
 
                 try
                 {
@@ -295,9 +311,18 @@ namespace GetStoreApp.Services.Controls.Download
 
                     try
                     {
-                        if (BitsDict.ContainsKey(callback.DownloadID))
+                        if (BitsDict.TryGetValue(callback.DownloadID.ToString(), out (string saveFilePath, IBackgroundCopyJob backgroundCopyJob, BackgroundCopyCallback backgroundCopyCallback) downloadValue))
                         {
-                            BitsDict.Remove(callback.DownloadID);
+                            DownloadProgress?.Invoke(new DownloadProgress()
+                            {
+                                DownloadID = callback.DownloadID.ToString(),
+                                DownloadProgressState = DownloadProgressState.Finished,
+                                FileName = Path.GetFileName(downloadValue.saveFilePath),
+                                FilePath = downloadValue.saveFilePath,
+                                DownloadSpeed = 0,
+                                CompletedSize = progress.BytesTransferred,
+                                TotalSize = progress.BytesTotal,
+                            });
                         }
                     }
                     catch (Exception e)
@@ -314,9 +339,47 @@ namespace GetStoreApp.Services.Controls.Download
                     LogService.WriteLog(LoggingLevel.Warning, "Finalize background intelligent transfer service download task failed", e);
                 }
             }
-            else if (state is BG_JOB_STATE.BG_JOB_STATE_SUSPENDED)
+            // 下载错误
+            else if (state is BG_JOB_STATE.BG_JOB_STATE_ERROR || state is BG_JOB_STATE.BG_JOB_STATE_TRANSIENT_ERROR)
             {
-                DownloadPaused?.Invoke(callback.DownloadID);
+                downloadJob.GetProgress(out BG_JOB_PROGRESS progress);
+
+                try
+                {
+                    callback.StatusChanged -= OnStatusChanged;
+                    downloadJob.Complete();
+
+                    bitsLock.Enter();
+
+                    try
+                    {
+                        if (BitsDict.TryGetValue(callback.DownloadID.ToString(), out (string saveFilePath, IBackgroundCopyJob backgroundCopyJob, BackgroundCopyCallback backgroundCopyCallback) downloadValue))
+                        {
+                            DownloadProgress?.Invoke(new DownloadProgress()
+                            {
+                                DownloadID = callback.DownloadID.ToString(),
+                                DownloadProgressState = DownloadProgressState.Finished,
+                                FileName = Path.GetFileName(downloadValue.saveFilePath),
+                                FilePath = downloadValue.saveFilePath,
+                                DownloadSpeed = 0,
+                                CompletedSize = progress.BytesTransferred,
+                                TotalSize = progress.BytesTotal,
+                            });
+                        }
+                    }
+                    catch (Exception e)
+                    {
+                        ExceptionAsVoidMarshaller.ConvertToUnmanaged(e);
+                    }
+                    finally
+                    {
+                        bitsLock.Exit();
+                    }
+                }
+                catch (Exception e)
+                {
+                    LogService.WriteLog(LoggingLevel.Warning, "Remove background intelligent transfer service failed download task failed", e);
+                }
             }
         }
     }
