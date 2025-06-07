450db37a769de0626d4a12b9cdcd60c77acfef03
高怡飞 <363301617@qq.com>
2025-06-07 11:02:27 +0800

    Add PackageVolumeDialog
    Add PackageVolumeDialog

---
+++ GetStoreApp/Views/Dialogs/PackageVolumeDialog.xaml.cs
@@ -0,0 +1,54 @@
+﻿using GetStoreApp.Models;
+using Microsoft.UI.Xaml.Controls;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+
+namespace GetStoreApp.Views.Dialogs
+{
+    /// <summary>
+    /// 应用包存放卷对话框
+    /// </summary>
+    public sealed partial class PackageVolumeDialog : ContentDialog, INotifyPropertyChanged
+    {
+        private PackageVolumeModel _currentPackageVolume;
+
+        public PackageVolumeModel CurrentPackageVolume
+        {
+            get { return _currentPackageVolume; }
+
+            set
+            {
+                if (!Equals(_currentPackageVolume, value))
+                {
+                    _currentPackageVolume = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(CurrentPackageVolume)));
+                }
+            }
+        }
+
+        private PackageVolumeModel _selectedPackageVolume;
+
+        public PackageVolumeModel SelectedPackageVolume
+        {
+            get { return _selectedPackageVolume; }
+
+            set
+            {
+                if (!Equals(_selectedPackageVolume, value))
+                {
+                    _selectedPackageVolume = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(SelectedPackageVolume)));
+                }
+            }
+        }
+
+        public ObservableCollection<PackageVolumeModel> PackageVolumeCollection = [];
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public PackageVolumeDialog()
+        {
+            InitializeComponent();
+        }
+    }
+}
