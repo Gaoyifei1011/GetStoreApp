0300ba809a3dbdf2c652d4b86c85415be867f376
高怡飞 <363301617@qq.com>
2025-06-08 13:31:14 +0800

    Update PackageVolumeDialog implementation
    Update PackageVolumeDialog implementation

--- GetStoreApp/Views/Dialogs/PackageVolumeDialog.xaml.cs
+++ GetStoreApp/Views/Dialogs/PackageVolumeDialog.xaml.cs
@@ -1,54 +1,180 @@
 ﻿using GetStoreApp.Models;
+using GetStoreApp.Services.Root;
+using GetStoreApp.Views.Pages;
+using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Navigation;
+using System;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
+using Windows.Foundation.Diagnostics;
+using Windows.Management.Deployment;
+using Windows.UI.Text;
+
+// 抑制 CA1822，IDE0060 警告
+#pragma warning disable CA1822,IDE0060
 
 namespace GetStoreApp.Views.Dialogs
 {
     /// <summary>
-    /// 应用包存放卷对话框
+    /// 应用包存储卷对话框
     /// </summary>
     public sealed partial class PackageVolumeDialog : ContentDialog, INotifyPropertyChanged
     {
-        private PackageVolumeModel _currentPackageVolume;
+        private readonly string PackageVolumeInfoString = ResourceService.GetLocalized("Dialog/PackageVolumeInfo");
+        private readonly string PackageVolumeAddString = ResourceService.GetLocalized("Dialog/PackageVolumeAdd");
+        private readonly PackageManager PackageManager;
+        private readonly PackageModel Package;
 
-        public PackageVolumeModel CurrentPackageVolume
+        private bool _isPrimaryEnabled;
+
+        public bool IsPrimaryEnabled
         {
-            get { return _currentPackageVolume; }
+            get { return _isPrimaryEnabled; }
 
             set
             {
-                if (!Equals(_currentPackageVolume, value))
+                if (!Equals(_isPrimaryEnabled, value))
                 {
-                    _currentPackageVolume = value;
-                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(CurrentPackageVolume)));
+                    _isPrimaryEnabled = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(IsPrimaryButtonEnabled)));
                 }
             }
         }
 
-        private PackageVolumeModel _selectedPackageVolume;
+        public List<Type> PageList { get; } = [typeof(PackageVolumeInfoPage), typeof(PackageVolumeAddPage)];
 
-        public PackageVolumeModel SelectedPackageVolume
-        {
-            get { return _selectedPackageVolume; }
-
-            set
-            {
-                if (!Equals(_selectedPackageVolume, value))
-                {
-                    _selectedPackageVolume = value;
-                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(SelectedPackageVolume)));
-                }
-            }
-        }
-
-        public ObservableCollection<PackageVolumeModel> PackageVolumeCollection = [];
+        public ObservableCollection<ContentLinkInfo> BreadCollection { get; } = [];
 
         public event PropertyChangedEventHandler PropertyChanged;
 
-        public PackageVolumeDialog()
+        public PackageVolumeDialog(PackageManager packageManager, PackageModel package)
         {
             InitializeComponent();
+            PackageManager = packageManager;
+            Package = package;
+        }
+
+        #region 第一部分：应用包存储卷对话框——挂载的事件
+
+        /// <summary>
+        /// 打开对话框时触发的事件
+        /// </summary>
+        private void OnOpened(object sender, ContentDialogOpenedEventArgs args)
+        {
+            PackageVolumeFrame.ContentTransitions = SuppressNavigationTransitionCollection;
+
+            // 第一次导航
+            if (GetCurrentPageType() is null)
+            {
+                NavigateTo(PageList[0], new List<object>() { this, PackageManager, Package }, null);
+            }
+        }
+
+        /// <summary>
+        /// 点击返回到应用包存储卷信息页面
+        /// </summary>
+        private void OnBackClicked(object sender, RoutedEventArgs args)
+        {
+            if (BreadCollection.Count is 2 && Equals(GetCurrentPageType(), PageList[1]))
+            {
+                NavigateTo(PageList[0], null, false);
+            }
+        }
+
+        /// <summary>
+        /// 单击痕迹栏条目时发生的事件
+        /// </summary>
+        private void OnItemClicked(object sender, BreadcrumbBarItemClickedEventArgs args)
+        {
+            if (args.Item is ContentLinkInfo contentLinkInfo && BreadCollection.Count is 2 && Equals(contentLinkInfo.SecondaryText, BreadCollection[0].SecondaryText))
+            {
+                NavigateTo(PageList[0], null, false);
+            }
+        }
+
+        /// <summary>
+        /// 导航完成后发生
+        /// </summary>
+        private void OnNavigated(object sender, NavigationEventArgs args)
+        {
+            if (BreadCollection.Count is 0 && Equals(GetCurrentPageType(), PageList[0]))
+            {
+                BreadCollection.Add(new ContentLinkInfo()
+                {
+                    DisplayText = PackageVolumeInfoString,
+                    SecondaryText = "PackageVolumeInfo"
+                });
+            }
+            else if (BreadCollection.Count is 1 && Equals(GetCurrentPageType(), PageList[1]))
+            {
+                BreadCollection.Add(new ContentLinkInfo()
+                {
+                    DisplayText = PackageVolumeAddString,
+                    SecondaryText = "PackageVolumeAdd"
+                });
+            }
+            else if (BreadCollection.Count is 2 && Equals(GetCurrentPageType(), PageList[0]))
+            {
+                BreadCollection.RemoveAt(1);
+            }
+        }
+
+        /// <summary>
+        /// 导航失败时发生
+        /// </summary>
+        private void OnNavigationFailed(object sender, NavigationFailedEventArgs args)
+        {
+            args.Handled = true;
+            LogService.WriteLog(LoggingLevel.Error, nameof(GetStoreApp), nameof(PackageVolumeDialog), nameof(OnNavigationFailed), 1, args.Exception);
+        }
+
+        #endregion 第一部分：应用包存储卷对话框——挂载的事件
+
+        /// <summary>
+        /// 页面向前导航
+        /// </summary>
+        public void NavigateTo(Type navigationPageType, object parameter = null, bool? slideDirection = null)
+        {
+            try
+            {
+                if (slideDirection.HasValue)
+                {
+                    PackageVolumeFrame.ContentTransitions = slideDirection.Value ? RightSlideNavigationTransitionCollection : LeftSlideNavigationTransitionCollection;
+                }
+
+                // 导航到该项目对应的页面
+                PackageVolumeFrame.Navigate(navigationPageType, parameter);
+            }
+            catch (Exception e)
+            {
+                LogService.WriteLog(LoggingLevel.Error, nameof(GetStoreApp), nameof(PackageVolumeDialog), nameof(NavigateTo), 1, e);
+            }
+        }
+
+        /// <summary>
+        /// 获取能否返回到应用包存储卷信息页面
+        /// </summary>
+        private bool GetCanBack(int count)
+        {
+            return count > 1;
+        }
+
+        /// <summary>
+        /// 获取当前导航到的页
+        /// </summary>
+        private Type GetCurrentPageType()
+        {
+            return PackageVolumeFrame.CurrentSourcePageType;
+        }
+
+        /// <summary>
+        /// 获取当前导航控件内容对应的页面
+        /// </summary>
+        public object GetFrameContent()
+        {
+            return PackageVolumeFrame.Content;
         }
     }
 }
