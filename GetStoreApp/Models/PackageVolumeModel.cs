6549bb54ef3ee0c2c88ad49a96e5e9356205f808
高怡飞 <363301617@qq.com>
2025-06-08 13:28:36 +0800

    Update PackageVolumeModel
    Update PackageVolumeModel

--- GetStoreApp/Models/PackageVolumeModel.cs
+++ GetStoreApp/Models/PackageVolumeModel.cs
@@ -6,7 +6,7 @@ namespace GetStoreApp.Models
     /// <summary>
     /// 磁盘卷数据模型
     /// </summary>
-    public partial class PackageVolumeModel : INotifyPropertyChanged
+    public class PackageVolumeModel
     {
         /// <summary>
         /// 存储卷名称
@@ -14,24 +14,24 @@ namespace GetStoreApp.Models
         public string Name { get; set; }
 
         /// <summary>
+        /// 存储卷空间
+        /// </summary>
+        public string Space { get; set; }
+
+        /// <summary>
+        /// 存储卷 ID
+        /// </summary>
+        public string PackageVolumeId { get; set; }
+
+        /// <summary>
         /// 存储卷路径
         /// </summary>
         public string PackageVolumePath { get; set; }
 
         /// <summary>
-        /// 存储卷可用空间
+        /// 存储卷已使用空间百分比
         /// </summary>
-        public ulong PackageVolumeAvailableSpace { get; set; }
-
-        /// <summary>
-        /// 存储卷所有空间
-        /// </summary>
-        public ulong PackageVolumeTotalSpace { get; set; }
-
-        /// <summary>
-        /// 存储卷可用空间百分比
-        /// </summary>
-        public double PackageVolumeAvailablePercentage { get; set; }
+        public double PackageVolumeUsedPercentage { get; set; }
 
         /// <summary>
         /// 存储卷
@@ -46,41 +46,11 @@ namespace GetStoreApp.Models
         /// <summary>
         /// 存储卷可用空间警告（可用空间在 5% - 10%）
         /// </summary>
-        private bool _isAvailableSpaceWarning;
-
-        public bool IsAvailableSpaceWarning
-        {
-            get { return _isAvailableSpaceWarning; }
-
-            set
-            {
-                if (!Equals(_isAvailableSpaceWarning, value))
-                {
-                    _isAvailableSpaceWarning = value;
-                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(IsAvailableSpaceWarning)));
-                }
-            }
-        }
+        public bool IsAvailableSpaceWarning { get; set; }
 
         /// <summary>
         /// 存储空间是否不可用（可用空间在 0% - 5%）
         /// </summary>
-        private bool _isAvailableSpaceError;
-
-        public bool IsAvailableSpaceError
-        {
-            get { return _isAvailableSpaceError; }
-
-            set
-            {
-                if (!Equals(_isAvailableSpaceError, value))
-                {
-                    _isAvailableSpaceError = value;
-                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(IsAvailableSpaceError)));
-                }
-            }
-        }
-
-        public event PropertyChangedEventHandler PropertyChanged;
+        public bool IsAvailableSpaceError { get; set; }
     }
 }
