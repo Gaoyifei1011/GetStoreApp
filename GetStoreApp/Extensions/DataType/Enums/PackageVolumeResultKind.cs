2a0eae5bb09ebb2a6dcf3eaf112a079b07e7c800
高怡飞 <363301617@qq.com>
2025-06-08 13:28:55 +0800

    Add PackageVolumeResultKind
    Add PackageVolumeResultKind

---
+++ GetStoreApp/Extensions/DataType/Enums/PackageVolumeResultKind.cs
@@ -0,0 +1,10 @@
+﻿namespace GetStoreApp.Extensions.DataType.Enums
+{
+    public enum PackageVolumeResultKind
+    {
+        Empty = 0,
+        Loading = 1,
+        Operating = 2,
+        Successfully = 3
+    }
+}
