3cc467d24d40e6bfe1feb18524016bba5e859e91
高怡飞 <363301617@qq.com>
2025-06-30 22:55:50 +0800

    Add ControlItemModel
    Add ControlItemModel

---
+++ GetStoreApp/Models/ControlItemModel.cs
@@ -0,0 +1,28 @@
+﻿namespace GetStoreApp.Models
+{
+    /// <summary>
+    /// 主页面项目数据模型
+    /// </summary>
+    public sealed class ControlItemModel
+    {
+        /// <summary>
+        /// 主页面项目标题
+        /// </summary>
+        public string Title { get; set; }
+
+        /// <summary>
+        /// 主页面项目描述
+        /// </summary>
+        public string Description { get; set; }
+
+        /// <summary>
+        /// 主页面项目图片路径
+        /// </summary>
+        public string ImagePath { get; set; }
+
+        /// <summary>
+        /// 主页面项目标签
+        /// </summary>
+        public string Tag { get; set; }
+    }
+}
