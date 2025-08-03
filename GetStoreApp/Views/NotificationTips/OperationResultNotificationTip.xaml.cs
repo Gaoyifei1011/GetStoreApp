f09a81c91c54e06e086c7a7893e13245c1f0ad29
怡飞 高 <363301617@qq.com>
2025-08-03 11:59:15 +0800

    Add TeachingTip DesktopAcrylicBackdrop support
    Add TeachingTip DesktopAcrylicBackdrop support

--- GetStoreApp/Views/NotificationTips/OperationResultNotificationTip.xaml.cs
+++ GetStoreApp/Views/NotificationTips/OperationResultNotificationTip.xaml.cs
@@ -1,7 +1,15 @@
-﻿using GetStoreApp.Extensions.DataType.Enums;
+﻿using GetStoreApp.Extensions.Backdrop;
+using GetStoreApp.Extensions.DataType.Enums;
 using GetStoreApp.Services.Root;
+using Microsoft.UI.Composition;
+using Microsoft.UI.Content;
+using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Hosting;
+using System;
 using System.ComponentModel;
+using System.Numerics;
+using System.Runtime.InteropServices.Marshalling;
 
 namespace GetStoreApp.Views.NotificationTips
 {
@@ -10,6 +18,11 @@ namespace GetStoreApp.Views.NotificationTips
     /// </summary>
     public sealed partial class OperationResultNotificationTip : TeachingTip, INotifyPropertyChanged
     {
+        private bool isConnected;
+        private Grid visualGrid;
+        private Grid nonHeroContentRootGrid;
+        private ContentExternalBackdropLink contentExternalBackdropLink;
+
         private bool _isSuccessOperation;
 
         public bool IsSuccessOperation
@@ -231,6 +244,77 @@ namespace GetStoreApp.Views.NotificationTips
                 {
                     IsSuccessOperation = false;
                     OperationContent = reason;
+                }
+            }
+        }
+
+        ~OperationResultNotificationTip()
+        {
+            if (nonHeroContentRootGrid is not null)
+            {
+                try
+                {
+                    nonHeroContentRootGrid.Loaded -= OnLoaded;
+                }
+                catch (Exception e)
+                {
+                    ExceptionAsVoidMarshaller.ConvertToUnmanaged(e);
+                }
+            }
+        }
+
+        /// <summary>
+        /// 自定义控件样式
+        /// </summary>
+        protected override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+            nonHeroContentRootGrid = GetTemplateChild("NonHeroContentRootGrid") as Grid;
+            if (nonHeroContentRootGrid is not null)
+            {
+                nonHeroContentRootGrid.Loaded += OnLoaded;
+            }
+        }
+
+        /// <summary>
+        /// 控件加载完成后触发的事件
+        /// </summary>
+        private void OnLoaded(object sender, RoutedEventArgs args)
+        {
+            if (!ControlBackdropController.IsClosed && sender is Grid grid)
+            {
+                if (!isConnected)
+                {
+                    isConnected = true;
+                    visualGrid = new();
+                    Canvas.SetZIndex(visualGrid, -9999);
+                    grid.Children.Add(visualGrid);
+
+                    contentExternalBackdropLink = ControlBackdropController.CreateContentExternalBackdropLink();
+
+                    if (contentExternalBackdropLink is not null)
+                    {
+                        contentExternalBackdropLink.ExternalBackdropBorderMode = CompositionBorderMode.Soft;
+                        contentExternalBackdropLink.PlacementVisual.Size = grid.ActualSize;
+                        contentExternalBackdropLink.PlacementVisual.Clip = contentExternalBackdropLink.PlacementVisual.Compositor.CreateRectangleClip(0, 0, grid.ActualSize.X, grid.ActualSize.Y,
+                            new Vector2(Convert.ToSingle(CornerRadius.TopLeft), Convert.ToSingle(CornerRadius.TopLeft)),
+                            new Vector2(Convert.ToSingle(CornerRadius.TopRight), Convert.ToSingle(CornerRadius.TopRight)),
+                            new Vector2(Convert.ToSingle(CornerRadius.BottomRight), Convert.ToSingle(CornerRadius.BottomRight)),
+                            new Vector2(Convert.ToSingle(CornerRadius.BottomLeft), Convert.ToSingle(CornerRadius.BottomLeft)));
+                        ElementCompositionPreview.SetElementChildVisual(visualGrid, contentExternalBackdropLink.PlacementVisual);
+                    }
+                }
+                else
+                {
+                    if (contentExternalBackdropLink is not null)
+                    {
+                        contentExternalBackdropLink.PlacementVisual.Size = grid.ActualSize;
+                        contentExternalBackdropLink.PlacementVisual.Clip = contentExternalBackdropLink.PlacementVisual.Compositor.CreateRectangleClip(0, 0, grid.ActualSize.X, grid.ActualSize.Y,
+                            new Vector2(Convert.ToSingle(CornerRadius.TopLeft), Convert.ToSingle(CornerRadius.TopLeft)),
+                            new Vector2(Convert.ToSingle(CornerRadius.TopRight), Convert.ToSingle(CornerRadius.TopRight)),
+                            new Vector2(Convert.ToSingle(CornerRadius.BottomRight), Convert.ToSingle(CornerRadius.BottomRight)),
+                            new Vector2(Convert.ToSingle(CornerRadius.BottomLeft), Convert.ToSingle(CornerRadius.BottomLeft)));
+                    }
                 }
             }
         }
