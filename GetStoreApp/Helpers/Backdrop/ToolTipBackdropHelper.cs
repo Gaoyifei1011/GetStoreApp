7d54389a708acde33a2592ae10c96a55ce0e93a7
怡飞 高 <363301617@qq.com>
2025-08-03 17:24:06 +0800

    Add ToolTipBackdropHelper
    Add ToolTipBackdropHelper

---
+++ GetStoreApp/Helpers/Backdrop/ToolTipBackdropHelper.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Media;
+
+namespace GetStoreApp.Helpers.Backdrop
+{
+    /// <summary>
+    /// 工具提示背景色辅助类
+    /// </summary>
+    public static class ToolTipBackdropHelper
+    {
+        /// <summary>
+        /// 工具提示加载完成后触发的事件
+        /// </summary>
+        public static void OnLoaded(object sender,RoutedEventArgs args)
+        {
+            if(sender is ToolTip toolTip && toolTip.Parent is Popup popup)
+            {
+                popup.RequestedTheme = toolTip.RequestedTheme;
+                popup.SystemBackdrop ??= Application.Current.Resources["AcrylicBackgroundFillColorDefaultBackdrop"] as SystemBackdrop;  
+            }
+        }
+    }
+}
